155ce3d02cf4daf462153e3424e360c20164b450
Wang Ru <rockidog@live.com>
2022-03-05 01:03:39 +0800

    Fix bottle version

--- cheers-project/cmake/import_bottle.cmake
+++ cheers-project/cmake/import_bottle.cmake
@@ -1,6 +1,6 @@
 FetchContent_Declare(ru-wang.bottle
   GIT_REPOSITORY git@github.com:ru-wang/bottle.git
-  GIT_TAG        origin/master
+  GIT_TAG        v0.1.0
   GIT_SHALLOW    TRUE)
 FetchContent_MakeAvailable(ru-wang.bottle)
 
